b6488cdd0f7bd0bf2bc0232c6fedcae7505ecafc
Kristof Schröder <kristof_schroeder@web.de>
2020-10-25 15:25:26 +0100

    Improve docu for regularization classes

--- src/hmirls/regularization.py
+++ src/hmirls/regularization.py
@@ -58,9 +58,9 @@ class FixedRankSpectralShiftRegularizationRule(RegularizationRule):
 
         .. math::
 
-            R_1 : \\mathbb{C}^{d_1 \\times d_1} \\rightarrow \\mathbb{C}^{d_1 \\times d_1}, x \\mapsto xx^{\\star}+\\varepsilon \cdot Id_{d_1},
+            R_1 : \\mathbb{C}^{d_1 \\times d_1} \\rightarrow \\mathbb{C}^{d_1 \\times d_1}, x \\mapsto xx^{\\star}+\\varepsilon^2 \cdot Id_{d_1},
 
-            R_2 : \\mathbb{C}^{d_2 \\times d_2} \\rightarrow \\mathbb{C}^{d_2 \\times d_2}, x \\mapsto x^{\\star}x +\\varepsilon \cdot Id_{d_2}.
+            R_2 : \\mathbb{C}^{d_2 \\times d_2} \\rightarrow \\mathbb{C}^{d_2 \\times d_2}, x \\mapsto x^{\\star}x +\\varepsilon^2 \cdot Id_{d_2}.
 
         Spectral shift :math:`\\varepsilon` is given by
 
@@ -68,7 +68,7 @@ class FixedRankSpectralShiftRegularizationRule(RegularizationRule):
 
             \\varepsilon = \\min(\\max(\\sigma_{\\text{rank_estimate}}(x), \\text{minimal_shift}), \\text{shift_parameter}),
 
-        where :math:`\sigma_{\\text{rank_estimate}}(x)` is the rank_estimate-th singular value of x.
+        where :math:`\\sigma_{\\text{rank_estimate}}(x)` is the rank_estimate-th singular value of x.
 
         :param x: array of shape :math:`(d_1, d_2)`
         :param schatten_p_parameter:
